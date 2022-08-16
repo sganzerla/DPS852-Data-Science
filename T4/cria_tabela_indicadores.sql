c5515d5e49d40531e0bc3e67f3a1ff5642637ed2
alisson.sganzerla <alisson.sganzerla@gmailcom>
2022-08-16 12:19:11 -0300

    T4

---
+++ T4/cria_tabela_indicadores.sql
@@ -0,0 +1,5 @@
+CREATE TABLE indicadores (
+	indicador_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	codigo text NOT NULL,
+	nome text NOT NULL
+);
---
+++ T4/cria_tabela_pais.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE paises (
+	pais_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	nome_curto text NOT NULL,
+	nome_longo text NOT NULL,
+	regiao text NOT NULL
+);

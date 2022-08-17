616547e480d969af96a52152c495fe39e5d9a661
alisson.sganzerla <alisson.sganzerla@gmailcom>
2022-08-16 22:44:49 -0300

    chore: paises insert

--- T4/cria_tabela_pais.sql
+++ T4/cria_tabela_pais.sql
@@ -3,5 +3,6 @@ CREATE TABLE paises (
 	pais_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 	nome_curto text NOT NULL,
 	nome_longo text NOT NULL,
-	regiao text NOT NULL
+	regiao text NOT NULL,
+	UNIQUE(nome_curto)
 );

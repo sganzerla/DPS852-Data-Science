305397cddabbc2fe5c7768b571e6d337ba99c98d
alisson.sganzerla <alisson.sganzerla@gmailcom>
2022-08-18 18:28:13 -0300

    refactor: criando tabelas sem arquivo externo

--- T4/cria_tabela_indicadores.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE indicadores (
-	indicador_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	codigo text NOT NULL,
-	nome text NOT NULL,
-	UNIQUE(codigo)
-);
--- T4/cria_tabela_pais.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE paises (
-	pais_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	nome_curto text NOT NULL,
-	nome_longo text NOT NULL,
-	regiao text NOT NULL,
-	UNIQUE(nome_curto)
-);
